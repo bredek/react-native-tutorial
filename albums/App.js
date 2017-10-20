e0c199e93f7c489a36560ad6753f8c9738df0ca0
eugene.bredyuk <eugene.bredyuk@gmail.com>
2017-10-20 17:40:48 +0300

    First element rendering

--- albums/App.js
+++ albums/App.js
@@ -1,2 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
 import React from 'react';
-import ReactNative from 'react-native'
+import {
+  Text,
+  AppRegistry
+} from 'react-native';
+
+export default App = () => {
+    return (
+        <Text >
+          Some text here
+        </Text>
+    );
+}
