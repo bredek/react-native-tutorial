cf3f02a4a9aaede60c59b6aaace91377a509dc9c
eugene.bredyuk <eugene.bredyuk@gmail.com>
2017-10-23 15:40:40 +0300

    Refactoring LoginForm into a State Driven component

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { TextInput, Text, Picker } from "react-native";
 import { Button, Card, CardSection } from "./common";
 
 class LoginForm extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      text: ""
+    }
+  }
   render() {
     return (
       <Card>
         <CardSection>
-          <Text>Hello!</Text>
+          <TextInput 
+          value={this.state.text}
+          onChangeText={(text)=>{
+            this.setState({text});
+            console.log(this.state.text);
+          }}
+          style={{ height: 20, width: 100 }} 
+          />
         </CardSection>
         <CardSection>
           <Text>Hello!</Text>
