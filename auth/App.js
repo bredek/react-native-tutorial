43f73ef44e54ba32d3aeb892203c5374a8ce7119
eugene.bredyuk <eugene.bredyuk@gmail.com>
2017-10-23 13:42:03 +0300

    Created LoginInput and connected firebase

--- auth/App.js
+++ auth/App.js
@@ -6,14 +6,29 @@
 
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
+import firebase from "firebase";
 
 import { Header } from "./src/components/common";
+import LoginForm from "./src/components/LoginForm";
 
 export default class App extends Component {
+  componentWillMount() {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyACmcGmTSsBD2geU1AkjfnC1cSqN1O163Q",
+      authDomain: "auth-92d83.firebaseapp.com",
+      databaseURL: "https://auth-92d83.firebaseio.com",
+      projectId: "auth-92d83",
+      storageBucket: "auth-92d83.appspot.com",
+      messagingSenderId: "685518308669"
+    };
+    firebase.initializeApp(config);
+  }
   render() {
     return (
       <View>
         <Header>Hello!</Header>
+        <LoginForm />
       </View>
     );
   }
