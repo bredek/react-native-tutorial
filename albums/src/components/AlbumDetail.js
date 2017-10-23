10bf43d9049342fd2477ed973e6688b9685a9b72
eugene.bredyuk <eugene.bredyuk@gmail.com>
2017-10-23 11:34:22 +0300

    Added some styling

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,14 +1,63 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = props => (
-  <Card>
-    <CardSection>
-      <Text>{props.album.title}</Text>
-    </CardSection>
-  </Card>
-);
+const styles = {
+  headerStyle: {
+    justifyContent: 'space-around',
+    flexDirection: 'column',
+  },
+  headerTextStyle: {
+    fontSize: 20,
+  },
+  thumbnailStyle: {
+    width: 50,
+    height: 50,
+  },
+  imageStyle: {
+    height: 300,
+    width: '100%',
+  },
+  imageContainerStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+};
+
+const AlbumDetail = ({ album }) => {
+  const {
+    title,
+    artist,
+    thumbnail_image,
+    image,
+  } = album;
+  const {
+    imageStyle,
+    thumbnailStyle,    
+    imageContainerStyle,
+    headerStyle,
+    headerTextStyle,
+  } = styles;
+
+  return (
+    <Card>
+      <CardSection>
+        <View style={imageContainerStyle}>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={headerStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image style={imageStyle} source={{ uri: image }} />
+      </CardSection>
+    </Card>
+  );
+};
 
 export default AlbumDetail;
